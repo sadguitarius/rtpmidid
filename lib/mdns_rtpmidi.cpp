9df48bc3559b50fd3d872a3fbcdfb37bffc983c5
sadguitarius <justbecuz@gmail.com>
2024-05-20 17:22:09 -0700

    include std algorithm

--- lib/mdns_rtpmidi.cpp
+++ lib/mdns_rtpmidi.cpp
@@ -17,6 +17,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
+#include <algorithm>
+
 #include <rtpmidid/logger.hpp>
 #include <rtpmidid/mdns_rtpmidi.hpp>
 #include <rtpmidid/poller.hpp>
